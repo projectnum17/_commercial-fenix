9a8668a8d827a2d43a00079a70a543c1709c4799
projectnum17 <egor.chornyy@gmail.com>
2023-10-06 16:40:12 +0300

    commit

--- src/js/modules/fixedHeaderMain.js
+++ src/js/modules/fixedHeaderMain.js
@@ -8,3 +8,34 @@ window.addEventListener("scroll", function () {
     header.classList.remove("header-fixed__bg")
   }
 })
+
+if (document.getElementById("header-white")) {
+  window.addEventListener("scroll", function () {
+    var headerWhite = document.getElementById("header-white")
+    var headerWhiteBtn = document.querySelector(".header-btn__white")
+    var defautLogo = document.getElementById("default-logo")
+    var newLogo = document.getElementById("new-logo")
+    var defaultBurger = document.getElementById("default-burger")
+    var newBurger = document.getElementById("new-burger")
+
+    if (window.pageYOffset > 1) {
+      //white heaeder
+      headerWhite.classList.add("header-white__new")
+      headerWhiteBtn.classList.add("header-btn__white-new")
+      defautLogo.style.display = "none"
+      newLogo.style.display = "block"
+
+      defaultBurger.style.display = "none"
+      newBurger.style.display = "block"
+    } else {
+      //white header
+      headerWhite.classList.remove("header-white__new")
+      headerWhiteBtn.classList.remove("header-btn__white-new")
+      defautLogo.style.display = "block"
+      newLogo.style.display = "none"
+
+      defaultBurger.style.display = "block"
+      newBurger.style.display = "none"
+    }
+  })
+}
